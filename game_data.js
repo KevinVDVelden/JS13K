2d0c9a9f1b17fa6cd9708d3a7a022e4c53626574
Kevin van der Velden <kevin.vdvelden@gmail.com>
2015-09-07 19:56:11 +0200

    Added piles of shiny shiny coins.
    Minified JS is 17147 bytes.
    Substituted JS is 74219 bytes.
    Uglified JS is 16823 bytes.
    Unminified JS files:
    40132 mjs.js
     1505 util.js
     1562 util_gl.js
     2579 flowmap.js
     9095 game_pcg.js
     4457 game_data.js
    17571 game.js
    76901 total
    Extra non-js files:
     988 ./bg.fs
     205 ./tiles.fs
      68 ./bg.vs
     165 ./tiles.vs
    4560 intro.html
      78 about.html
     207 menu.html
    6271 total
    Final compression:
    -rwx---r-x 1 Kevin Kevin 9341 Sep  7 19:56 js13k.zip

--- game_data.js
+++ game_data.js
@@ -1,16 +1,22 @@
 /** @const */ STAT_FROZEN = 0;
 /** @const */ STAT_FEAR = 1;
 /** @const */ STAT_BORED = 2;
+/** @const */ STAT_LOOT_VALUE = 3;
 
-/** @const */ TAG_THIEF = 0;
+/** @const */ TAG_ENT_TYPE = 0;
 /** @const */ TAG_ICON = 1;
 /** @const */ TAG_TILE = 2;
 /** @const */ TAG_THOUGHT = 3;
-/** @const */ TAG_TRIGGER_ACTION = 4;
+/** @const */ TAG_TILE_ICON = 4;
+/** @const */ TAG_TRIGGER_ACTION = 5;
 
 /** @const */ TAG_TRIGGER = 1000;
 /** @const */ TAG_TILE_BASE = 1001;
 /** @const */ TAG_THOUGHT_ALPHA = 1002;
+
+/** @const */ TYPE_THIEF = 1;
+/** @const */ TYPE_LOOT = 2;
+/** @const */ TYPE_TRAP = 3;
 
 /** @const */ ENTITY_X = 0;
 /** @const */ ENTITY_Y = 1;
@@ -21,11 +27,16 @@
 /** @const */ ENTITY_CHANGE_ATTRIBUTE = 6;
 /** @const */ ENTITY_TAGS = 7;
 /** @const */ ENTITY_THINK = 8;
-/** @const */ var THIEF_BASE =   [ [[0,0,64,-1],[0,0,10,-0.01],[0,0,10,0.001]], [1,3*8+3,,], [thiefThink] ];
+/** @const */ var THIEF_BASE =   [ [[0,0,64,-1],[0,0,10,-0.01],[0,0,10,0.001]], [TYPE_THIEF,3*8+3,,], [thiefThink] ];
 /** @const */ var TRAP_BASES = { 
-    0: [ [[40,0,40,-1]], [,,0*8+8,,trapTriggerNeighbours], [trapThink] ],
-    1: [ [[60,0,60,-1]], [,,0*8+7,,trapFreeze], [trapThink] ],
-    2: [ [[70,0,70,-1]], [,,0*8+9,,trapZap], [trapThink] ],
+    0: [ [[40,0,40,-1]], [TYPE_TRAP,,0*8+8,,,trapTriggerNeighbours], [trapThink] ],
+    1: [ [[60,0,60,-1]], [TYPE_TRAP,,0*8+7,,,trapFreeze], [trapThink] ],
+    2: [ [[70,0,70,-1]], [TYPE_TRAP,,0*8+9,,,trapZap], [trapThink] ],
+};
+/** @const */ var LOOT = {
+    0: [ [,,,[10,0,10,0.01]],             [TYPE_LOOT,,,,5*8+0], [] ],
+    1: [ [,,,[20,0,40,0.01]],             [TYPE_LOOT,,,,5*8+3], [] ],
+    2: [ [,,,[40,0,40,0.01]],             [TYPE_LOOT,,,,5*8+7], [] ],
 };
 /** @const */ var TRAP_NAMES = [ 'Alarm TDS&copy;', 'Freeze TDS&copy;', 'Zap TDS&copy;' ];
 
@@ -49,7 +60,7 @@ function trapThink( world, ent ) {
     var entities = world.entsAtPos( ent[0], ent[1] );
 
     for ( var i = 0; i < entities.length; i++ ) {
-        if ( !entities[i][ENTITY_TAGS][TAG_THIEF] ) continue;
+        if ( !entities[i][ENTITY_TAGS][TAG_ENT_TYPE] == TYPE_THIEF ) continue;
         if ( Math.abs( entities[i][0]-ent[0] ) + Math.abs( entities[i][1]-ent[1] ) < 1 ) {
             ent[ENTITY_TAGS][TAG_TRIGGER_ACTION]( world, ent, entities[i] );
         }

2cb2058281cd67efbdb8bd338c6eebe72619e7d3
Kevin van der Velden <KevinVDVelden@users.noreply.github.com>
2015-09-09 19:45:59 +0200

    Improved pathfinding.

--- flowmap.js
+++ flowmap.js
@@ -1,11 +1,17 @@
+/**@const */ var MAP_SIZE = 128;
+
 var Flowmap_Dirs = [-1,1,MAP_SIZE,-MAP_SIZE];
 function XY( i ) {
     return [ i % MAP_SIZE, ( i / MAP_SIZE ) | 0 ];
 }
-Flowmap = function( weights, targetX, targetY ) {
+Flowmap = function( weights, isValid ) {
     this.weights = weights;
+    this.isValid = isValid;
 
     this.lookQueue = [];
+    this.lookSet = {};
+    this.visited = [];
+    for ( var i = 0; i < MAP_SIZE*MAP_SIZE; i++ ) this.visited[i] = 0;
 
     this.map = [];
     this.map[MAP_SIZE*MAP_SIZE+1]=0;
@@ -13,6 +19,7 @@ Flowmap = function( weights, targetX, targetY ) {
 Flowmap.prototype.addTarget = function( x, y ) {
     var i = x+y*MAP_SIZE;
     this.lookQueue[this.lookQueue.length] = [ i, x, y, 99999, 0 ];
+    this.lookSet[i] = true;
 }
 Flowmap.prototype.bestNeighbour = function Flowmap_bestNeighbour( i ) {
     var closeI = -1;
@@ -33,9 +40,11 @@ Flowmap.prototype.directionFrom = function Flowmap_directionFrom( tX, tY ) {
     var map = this.map;
 
     var lookQueue = this.lookQueue;
+    var lookSet = this.lookSet;
     I = this.I;
     var targetX = this.targetX; var targetY = this.targetY;
     var weights = this.weights;
+    isValid = this.isValid;
 
     var lookI = I(tX,tY);
     if ( map[lookI] ) {
@@ -45,43 +54,64 @@ Flowmap.prototype.directionFrom = function Flowmap_directionFrom( tX, tY ) {
         if ( x < 0 || y < 0 || x > MAP_SIZE || y > MAP_SIZE ) return;
 
         var i = I( x, y );
-        if ( this.weights[i] == 0 ) return;
+        if ( !isValid( weights[ i ] ) || map[i] >= w ) return;
+        if ( lookSet[i] && lookSet[i] >= w ) {
+            return;
+        }
+        map[ i ] = w;
 
         lookQueue[lookQueue.length] = [ i, x, y, w, Math.abs( tX - x ) + Math.abs( tY - y ) ];
-        lookQueue.sort( function(b,a) { return a[4]==b[4] ? b[3]-a[3] : a[4]-b[4] } );
+        lookSet[ i ] = w;
     }
 
-    while ( lookQueue.length > 0 ) {
-        var first = lookQueue.pop();
-        console.log( first[1], first[2], first[3], first[4], first[0]-lookI);
+    var checked = 0;
+    while ( lookQueue.length > 0 && checked < 100000 ) {
+        checked += 1;
 
-        var w = 1;
+        var first = lookQueue.pop();
+        lookSet[ first[ 0 ] ] = false;
+
+        this.visited[ first[0] ] += 1;
+
+        //Get heighest neighbour
+        var newWeight = 1;
         for ( var dirI = 0; dirI < 4; dirI++ ) {
             var testI = first[0]+Flowmap_Dirs[dirI];
 
-            if ( map[ testI ] && map[ testI ] > w ) {
-                w = map[ testI ];
+            if ( map[ testI ] && map[ testI ] > newWeight ) {
+                newWeight = map[ testI ];
             }
         }
-        w -= 1;
+        //One removed from neighbour so decrement
+        newWeight -= 1;
 
-        if ( w == 0 ) {
-             map[ first[ 0 ] ] = first[ 3 ];
+        //No set neighbours, using the value from the insert
+        if ( newWeight == 0 ) {
+            map[ first[ 0 ] ] = first[ 3 ];
+            newWeight = first[ 3 ];
         } else {
-            var v = map[ first[ 0 ] ];
-            if ( v >= w + 1 ) continue;
-            map[ first[0] ] = w;
+            //Get current value
+            var curWeight = map[ first[ 0 ] ];
+
+            //If the current value is higher or equal (i.e. closer) than the new value, skip
+            if ( curWeight > newWeight ) {
+                continue;
+            }
+            map[ first[0] ] = newWeight;
         }
+
+        newWeight -= 1;
 
         for ( var dirI = 0; dirI < 4; dirI++ ) {
             var testI = first[0]+Flowmap_Dirs[dirI];
 
-            if ( !map[ testI ] || map[ testI ] < w ) {
+            if ( ( !map[ testI ] ) || map[ testI ] < newWeight ) {
                 var _x = testI % MAP_SIZE;
                 var _y = ( testI / MAP_SIZE ) | 0;
-                add( _x, _y, first[3] - 1 );
+                add( _x, _y, newWeight - 1 );
             }
         }
+        lookQueue.sort( function(b,a) { return a[3]!=b[3] ? b[3]-a[3] : a[4]-b[4] } );
 
         if ( lookI == first[0] ) break;
     }
